415d88ee623049468a3de9538e33dda1547c1871
semyon <odintsovvans@gmail.com>
2023-07-13 15:30:55 +0600

    hw_2

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,19 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
+
+
+    def __iter__(self):
+        current = self.top
+        while current is not None:
+            yield current.data
+            current = current.next_node
+
+
+    def __str__(self):
+        return "Stack is empty" if self.top is None else 'Stack: ' + ', '.join([str(item) for item in self])
+
 
     def push(self, data):
         """
@@ -23,7 +36,12 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node.next_node = self.top
+            self.top = new_node
 
     def pop(self):
         """
@@ -31,4 +49,9 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            raise Exception('Stack is empty')
+        data = self.top.data
+        self.top = self.top.next_node
+        return data
+
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,31 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+
+    def test_str(self):
+        self.assertEqual(str(self.stack), "Stack is empty")
+        self.stack.push('1')
+        self.stack.push('2')
+        self.stack.push('3')
+        self.assertEqual(str(self.stack), 'Stack: 3, 2, 1')
+
+
+    def test_push(self):
+        self.stack.push('1')
+        self.stack.push('2')
+        self.stack.push('3')
+        self.assertEqual(self.stack.top.data, '3')
+        self.assertEqual(self.stack.top.next_node.data, '2')
+        self.assertEqual(self.stack.top.next_node.next_node.data, '1')
+        self.assertEqual(self.stack.top.next_node.next_node.next_node, None)
+
+
+    def test_pop(self):
+        self.stack.push('1')
+        data = self.stack.pop()
+        self.assertEqual(data, '1')
