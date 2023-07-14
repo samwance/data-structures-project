59a97a920e7ba5b9da4228fbd99e4010b325e7b0
semyon <odintsovvans@gmail.com>
2023-07-14 14:29:17 +0600

    hw_5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,39 @@
+from src.queue import Queue
+
+
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
-class LinkedList:
+class LinkedList(Queue):
     """Класс для односвязного списка"""
+
+    def __init__(self):
+        super().__init__()
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +43,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+class TestLinkedList(unittest.TestCase):
+    def test_insert_beginning(self):
+        linked_list = LinkedList()
+        linked_list.insert_beginning({'T': 1})
+        linked_list.insert_beginning({'T': 2})
+        linked_list.insert_beginning({'T': 3})
+        self.assertEqual(str(linked_list), "{'T': 3} -> {'T': 2} -> {'T': 1} -> None")
+
+    def test_insert_at_end(self):
+        linked_list = LinkedList()
+        linked_list.insert_at_end({'T': 1})
+        linked_list.insert_at_end({'T': 2})
+        linked_list.insert_at_end({'T': 3})
+        self.assertEqual(str(linked_list), "{'T': 1} -> {'T': 2} -> {'T': 3} -> None")
+
+if __name__ == "__main__":
+    unittest.main()
